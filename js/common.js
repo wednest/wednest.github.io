0b236d8230998a14a5e27c9a4b5d0ca354c464eb
wednest <41552817+wednest@users.noreply.github.com>
2018-07-23 00:23:01 +0300

    Portfolio files

---
+++ js/common.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+
+  $(window).scroll(function () {
+    if ($(document).scrollTop() > 70) {
+      $('.topbar').addClass('fixed');
+    } else {
+      $('.topbar').removeClass('fixed');
+    }
+  }).scroll();
+
+  $(".topbar-burger").on('click', function () {
+    $(this).toggleClass("on");
+    $('.menu-section').toggleClass("on");
+  });
+
+  $('a[href^="#"]').click(function (event) {
+    var id_clicked_element = $(this).attr('href');
+    var destination = $(id_clicked_element).offset().top;
+    $('.topbar-burger').removeClass("on");
+    $('.menu-section').removeClass("on");
+    $('html, body').animate({ scrollTop: destination }, 500);
+    return false;
+  });
+
+});
