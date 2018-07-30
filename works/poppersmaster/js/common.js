b858b3413b27a7bbaff406772e50d6e21347d28d
wednest <41552817+wednest@users.noreply.github.com>
2018-07-30 03:05:30 +0300

    Add files via upload

--- works/poppersmaster/js/common.js
+++ works/poppersmaster/js/common.js
@@ -8,10 +8,10 @@ $(document).ready(function () {
     nav: true
   });
 
-  $(document).ready(function() {
-    $('.image-link').magnificPopup({type:'image'});
+  $(document).ready(function () {
+    $('.image-link').magnificPopup({ type: 'image' });
   });
-  
+
   // Добавление тени меню
   $(window).scroll(function () {
     if ($(document).scrollTop() > 84) {
@@ -89,61 +89,63 @@ $(document).ready(function () {
   // Увеличение уменьшение кол-ва товаров конец
 
   // Выделение ёмкости
-  $('#five, #fifteen, #thirty').click(function() {
+  $('#five, #fifteen, #thirty').click(function () {
     $(this).toggleClass('active');
   });
   // Выделение ёмкости Конец
 
   // Маска для ввода телефона
-  $("#phone").inputmask({"mask": "(999) 999-9999"});
+  $("#phone").inputmask({ "mask": "(999) 999-9999" });
   // Маска для ввода телефона Конец
 
   // Стилизация выпадающих списков
-  $('select').each(function(){
+  $('select').each(function () {
     var $this = $(this), numberOfOptions = $(this).children('option').length;
-  
-    $this.addClass('select-hidden'); 
+
+    $this.addClass('select-hidden');
     $this.wrap('<div class="select"></div>');
     $this.after('<div class="select-styled"></div>');
 
     var $styledSelect = $this.next('div.select-styled');
     $styledSelect.text($this.children('option').eq(0).text());
-  
-    var $list = $('<ul />', {
-        'class': 'select-options'
-    }).insertAfter($styledSelect);
-  
-    for (var i = 0; i < numberOfOptions; i++) {
-        $('<li />', {
-            text: $this.children('option').eq(i).text(),
-            rel: $this.children('option').eq(i).val()
-        }).appendTo($list);
-    }
-  
-    var $listItems = $list.children('li');
-  
-    $styledSelect.click(function(e) {
-        e.stopPropagation();
-        $('div.select-styled.active').not(this).each(function(){
-            $(this).removeClass('active').next('ul.select-options').hide();
-        });
-        $(this).toggleClass('active').next('ul.select-options').toggle();
-    });
-  
-    $listItems.click(function(e) {
-        e.stopPropagation();
-        $styledSelect.text($(this).text()).removeClass('active');
-        $this.val($(this).attr('rel'));
-        $list.hide();
-        //console.log($this.val());
-    });
-  
-    $(document).click(function() {
-        $styledSelect.removeClass('active');
-        $list.hide();
-    });
-    // Стилизация выпадающих списков Конец
 
-});
+    var $list = $('<ul />', {
+      'class': 'select-options'
+    }).insertAfter($styledSelect);
+
+    for (var i = 0; i < numberOfOptions; i++) {
+      $('<li />', {
+        text: $this.children('option').eq(i).text(),
+        rel: $this.children('option').eq(i).val()
+      }).appendTo($list);
+    }
+
+    var $listItems = $list.children('li');
+
+    $styledSelect.click(function (e) {
+      e.stopPropagation();
+      $('div.select-styled.active').not(this).each(function () {
+        $(this).removeClass('active').next('ul.select-options').hide();
+      });
+      $(this).toggleClass('active').next('ul.select-options').toggle();
+    });
+
+    $listItems.click(function (e) {
+      e.stopPropagation();
+      $styledSelect.text($(this).text()).removeClass('active');
+      $this.val($(this).attr('rel'));
+      $list.hide();
+      //console.log($this.val());
+    });
+
+    $(document).click(function () {
+      $styledSelect.removeClass('active');
+      $list.hide();
+    });
+
+  });
+  // Стилизация выпадающих списков Конец
+
+  
 
 });
