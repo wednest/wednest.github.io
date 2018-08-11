ff3e69f027c34868a7ce62deca86200786407480
wednest <41552817+wednest@users.noreply.github.com>
2018-08-11 15:44:30 +0300

    1.0

--- js/common.js
+++ js/common.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
   $(window).scroll(function () {
-    if ($(document).scrollTop() > 70) {
+    if ($(document).scrollTop() > 170) {
       $('.topbar').addClass('fixed');
     } else {
       $('.topbar').removeClass('fixed');
